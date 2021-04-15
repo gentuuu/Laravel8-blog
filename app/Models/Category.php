cc4104b578a1d0e72b98638ffcec9a4af7f7b38e
gentuuu <gentuuu1993@gmail.com>
2021-04-15 19:21:47 +0200

    Uzupełnie rodzice i dzieci

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Builder\FunctionLike;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function scopeOnlyParent($query){
+        return $query->whereNull('parent_id');
+    }
 
     public function children(){
         return $this->hasMany(self::class, 'parent_id');
