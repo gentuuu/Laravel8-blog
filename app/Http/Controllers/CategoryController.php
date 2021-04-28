d076cbba07479165bd10c5af7aba139acaa62dca
gentuuu <gentuuu1993@gmail.com>
2021-04-28 19:10:34 +0200

    Update kategoria

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -138,7 +138,28 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+
+          // proces validacji
+          $validator = Validator::make(
+            $request->all(),
+            [
+                'title' => 'required|string|max:60', 
+                'slug' => 'required|string|unique:categories,slug,'. $category->id, 
+                'thumbnail' => 'required', 
+                'description' => 'required|string|max:240', 
+            ],
+            [],
+            $this->attributes()
+        );
+
+        if ($validator->fails()){
+            if($request->has('parent_category')){
+                $request['parent_category'] = Category::select('id', 'title')->find($request->parent_category);
+            }
+            return redirect()->back()->withInput($request->all())->withErrors($validator);    
+        }
+        dd($request->all(),$category);
+
     }
 
     /**
