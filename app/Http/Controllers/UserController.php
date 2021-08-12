267980198716b97a30cf73198b013e5c3ff08efe
gentuuu <gentuuu1993@gmail.com>
2021-08-12 21:05:05 +0200

    Dodanie Alertu user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class UserController extends Controller
 {
@@ -54,6 +57,31 @@ class UserController extends Controller
         if ($validator->fails()){
             $request['role'] = Role::select('id', 'name')->find($request->role);
             return redirect()->back()->withInput($request->all())->withErrors($validator);
+        }
+
+        DB::beginTransaction();
+        try{
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            $user->assignRole($request->role);
+            Alert::success(
+                trans('users.alert.create.title'),
+                trans('users.alert.create.message.success')
+            );
+            return redirect()->route('users.index');
+        } catch(\Throwable $th){
+            DB::rollBack();
+            Alert::success(
+                trans('users.alert.create.title'),
+                trans('users.alert.create.message.error', ['error' => $th->getMessage()])
+            );
+            $request['role'] = Role::select('id', 'name')->find($request->role);
+            return redirect()->back()->withInput($request->all())->withErrors($validator);
+        } finally{
+            DB::commit();
         }
     }
 
